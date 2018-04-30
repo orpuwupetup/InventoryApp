b039d14e6145dd7047d25960581d36c5b402ad41
Cezary <cezarygraban@gmail.com>
2018-04-30 13:29:16 +0200

    Added functionality of changing quantity of product from the details Activity

--- app/src/main/java/com/example/orpuwupetup/inventoryapp/ProductDetailsActivity.java
+++ app/src/main/java/com/example/orpuwupetup/inventoryapp/ProductDetailsActivity.java
@@ -1,11 +1,15 @@
 package com.example.orpuwupetup.inventoryapp;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.orpuwupetup.inventoryapp.data.InventoryContract.InventoryEntry;
 
@@ -13,13 +17,18 @@ public class ProductDetailsActivity extends AppCompatActivity {
 
     TextView productName, price, quantity, suplierPhoneNumber, suplierName, description;
     ImageView productImage;
+    ImageButton incrementQuantity, decrementQuantity;
+    Uri productUri;
+
+    final private static int INCREMENT_QUANTITY = 1;
+    final private static int DECREMENT_QUANTITY = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_details);
 
-        Uri productUri;
+
         try {
             productUri = Uri.parse(getIntent().getExtras().getString("product_uri"));
         }catch (NullPointerException e){
@@ -34,17 +43,61 @@ public class ProductDetailsActivity extends AppCompatActivity {
         suplierName = findViewById(R.id.suplier_name);
         description = findViewById(R.id.description);
         productImage = findViewById(R.id.product_image);
+        incrementQuantity = findViewById(R.id.increment_quantity);
+        decrementQuantity = findViewById(R.id.decrement_quantity);
 
-        populateViews(productUri);
+        populateViews();
+
+        incrementQuantity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeQuantity(INCREMENT_QUANTITY);
+            }
+        });
+
+        decrementQuantity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeQuantity(DECREMENT_QUANTITY);
+            }
+        });
 
     }
 
-    private void populateViews(Uri uri){
+    private void changeQuantity(int whichButton){
+        String [] projection = {InventoryEntry.COLUMN_PRODUCT_QUANTITY};
+        Cursor cursor = getContentResolver().query(productUri,
+                projection,
+                null,
+                null,
+                null);
+        cursor.moveToFirst();
+        int currentQuantity = cursor.getInt(cursor.getColumnIndex(InventoryEntry.COLUMN_PRODUCT_QUANTITY));
+        int newQuantity = currentQuantity;
+        ContentValues values = new ContentValues();
+        if(whichButton == DECREMENT_QUANTITY) {
+            if(currentQuantity == 0){
+                Toast.makeText(this, "Can't sell more, nothings left", Toast.LENGTH_LONG).show();
+            }else {
+                newQuantity = currentQuantity - 1;
+            }
+        }else{
+            newQuantity = currentQuantity + 1;
+        }
+
+        if(currentQuantity != newQuantity){
+            values.put(InventoryEntry.COLUMN_PRODUCT_QUANTITY, newQuantity);
+            getContentResolver().update(productUri, values, null, null);
+            quantity.setText(String.valueOf(newQuantity));
+        }
+    }
+
+    private void populateViews(){
 
 
         // TODO: Display description and product image
         String [] projection = {"*"};
-        Cursor cursor = getContentResolver().query(uri,
+        Cursor cursor = getContentResolver().query(productUri,
                 projection,
                 null,
                 null,
